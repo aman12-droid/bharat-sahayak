0f6b1019e21fa461ac684e5e89c2cd4513139ad8
Aman Kumar <amanraj3710@gmail.com>
2026-05-30 18:38:05 +0530

    add root route fix

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("🚀 Bharat Sahayak backend is running");
+});
 
 app.post("/chat", async (req, res) => {
 
